968b085e3204e9f63dad9f91e20d1c54603db9d2
MilenaPC <milena771@hotmail.fr>
2024-01-04 16:12:46 +0100

    update command print and echo

--- src/main/java/co/simplon/java/cli/parser/Match.java
+++ src/main/java/co/simplon/java/cli/parser/Match.java
@@ -3,15 +3,7 @@ package co.simplon.java.cli.parser;
 public class Match {
 	
 	public static Boolean isCommandExist(String inputs) {
-		if(inputs.equals("hw")) {
-			return true;
-		}else if(inputs.equals("date")) {
-			return true;			
-		}else if(inputs.equals("time")) {
-			return true;
-		}else if(inputs.equals("datetime")) {
-			return true;
-		}else if(inputs.equals("print") || inputs.equals("echo")) {
+		if(inputs.equals("hw") || inputs.equals("date") || inputs.equals("time") || inputs.equals("datetime") || inputs.equals("print") || inputs.equals("echo")) {
 			return true;
 		}else {
 			return false;
